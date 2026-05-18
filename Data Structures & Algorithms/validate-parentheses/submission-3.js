4778eacc4bc2774ce74c9e80e63cf6f4addd52e1
Ashar Huda <149627985+ahuda07@users.noreply.github.com>
2026-05-18 15:41:44 -0700

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        const stack = [];
+        if(s.length === 1){
+            return false;
+        }
+        for(const char of s){
+            if("{([".includes(char)){
+                stack.push(char);
+            }else{
+                let topElement = stack[stack.length - 1];
+                if(("}".includes(char) && "{".includes(topElement)) || 
+                (")".includes(char) && "(".includes(topElement)) || 
+                ("]".includes(char) && "[".includes(topElement))){
+                    stack.pop(char)
+                }else{
+                    stack.push(char);
+                }
+            }
+        }
+        return(stack.length === 0);
+    }
+}
